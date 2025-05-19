2c03b040fc19be3455868e6c187513885d358f4d
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-05-19 13:25:39 +0100

    commit ?

--- db_connection/db_conn.php
+++ db_connection/db_conn.php
@@ -12,3 +12,31 @@ try {
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+<?php
+$host = "localhost"; // cuz WAMP server runs MySQL on localhost
+$dbname = "db_sonatrach_dp";
+$username = "root"; // hada Default WAMP MySQL username
+$password = "";
+try {
+    // Create a new PDO instance (PHP Data Objects)
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable error reporting
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+<?php
+$host = "localhost"; // cuz WAMP server runs MySQL on localhost
+$dbname = "db_sonatrach_dp";
+$username = "root"; // hada Default WAMP MySQL username
+$password = "";
+try {
+    // Create a new PDO instance (PHP Data Objects)
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable error reporting
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
