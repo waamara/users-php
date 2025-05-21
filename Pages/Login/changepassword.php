2102a88760e938b4d012726500d927db49de0238
Aymen Berbiche <138572701+waamara@users.noreply.github.com>
2025-05-21 13:57:22 +0100

    changes working

--- Pages/Login/changepassword.php
+++ Pages/Login/changepassword.php
@@ -700,9 +700,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <div class="omc">
 
                             <div class="file-upload-container">
+                                <label for="old_password" class="form-label">
+                                            Ajouter Votre Photo de Profil <span class="required-asterisk">*</span>
+                                </label>
                                 <label for="imageUpload" class="file-upload-label">
                                     <div class="upload-icon">
-                                        <!-- Simple CSS upload icon -->
                                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                                             <polyline points="17 8 12 3 7 8"></polyline>
